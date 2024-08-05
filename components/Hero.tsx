7ca4b893a3cc6f2ce37fdb97fbd0ea7d0b824365
dan <81313913+danxczm@users.noreply.github.com>
2024-08-05 12:24:23 +0300

    feat: iPhone video added - depends on screen size

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,25 +1,47 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import styled from '@emotion/styled';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+
 import { heroVideo, smallHeroVideo } from '@/utils';
 
 const Hero = () => {
+  const [videoSource, setVideoSource] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSource = () => {
+    if (window.innerWidth < 760) {
+      setVideoSource(smallHeroVideo);
+    } else {
+      setVideoSource(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleVideoSource);
+
+    return () => window.removeEventListener('resize', handleVideoSource);
+  }, []);
+
   useGSAP(() => {
     gsap.to('.hero-title', {
       opacity: 1,
       delay: 1.5,
     });
   }, []);
+
   return (
     <HeroSection className="nav-height">
       <HeroContainer className="flex-center">
         <p className="hero-title">iPhone 15 Pro</p>
         <HeroVideoContainer>
-          <video>
-            <source src={videoSrc} />
-          </video>
+          <HeroVideo className="pointer-events-none" autoPlay playsInline muted key={videoSource}>
+            <source src={videoSource} type="video/mp4" />
+          </HeroVideo>
         </HeroVideoContainer>
       </HeroContainer>
     </HeroSection>
@@ -47,4 +69,9 @@ const HeroVideoContainer = styled.div`
   @media (min-width: 768px) {
     width: 83.333333%;
   }
+`;
+
+const HeroVideo = styled.video`
+  width: 100%;
+  max-height: 350px;
 `;
